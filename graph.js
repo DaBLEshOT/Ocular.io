e371cb0bc17d36834745a7fea417e05162adf6b4
HanaMuzelj <hana.muzelj@student.um.si>
2021-04-02 20:46:32 +0200

    graph.js fixed visual side
    Ready for connection with BE, just visually changed, time to change functions. Might change it later ko bom vidla kak zgleda z legit podatki hih :D

--- graph.js
+++ graph.js
@@ -1,32 +1,21 @@
 window.onload = trackersPerWeek;
-
 function trackersPerWeek() {
-
     var ctx = document.getElementById('week');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: ['https://Google.com', 'https://Facebook.com', 'https://Twitter.com', 'https://BBCNews.org.com', 'https://TikTok.com', 'https://Tumbler.com', 'https://Amazon.com', 'https://MedOverNet.com'],
+            labels: ["tracker1", "tracker2", "tracker3", "tracker4","tracker5","tracker6"],
             datasets: [{
-                label: '8 Most common trackers in the last week',
-                data: [84.00, 72.00, 40.00, 10.00, 66.000, 33.000, 45.000, 86.000],
-                backgroundColor: [
-                    'rgba(216, 27, 96, 0.6)',
-                    'rgba(3, 169, 244, 0.6)',
-                    'rgba(255, 152, 0, 0.6)',
-                    'rgba(29, 233, 182, 0.6)',
-                    'rgba(156, 39, 176, 0.6)',
-                    'rgba(84, 110, 122, 0.6)'
-                ],
-                borderColor: [
-                    'rgba(216, 27, 96, 1)',
-                    'rgba(3, 169, 244, 1)',
-                    'rgba(255, 152, 0, 1)',
-                    'rgba(29, 233, 182, 1)',
-                    'rgba(156, 39, 176, 1)',
-                    'rgba(84, 110, 122, 1)'
-                ],
-                borderWidth: 1
+                label: '6 Most common trackers in the last week',
+                data: [84.00, 72.00, 40.00, 40.00, 66.000, 63.000],
+                backgroundColor: [ 
+                    'rgba(232,118,157,1)',  
+                    'rgba(228,97,141,1)',
+                    'rgba(225,75,126,1)',
+                    'rgba(221,53,110,1)',
+                    'rgba(213,36,96,1)',
+                    'rgba(191,32,86,1)'
+                ]
             }]
         },
         options: {
@@ -37,11 +26,11 @@ function trackersPerWeek() {
 
             title: {
                 display: true,
-                text: '8 Most common trackers in the last week',
+                text: '6 most common trackers in the last week',
                 position: 'top',
-                fontSize: 30,
-                fontFamily: 'Inconsolata , Arial, sans-serif',
-                fontColor: "#333333",
+                fontSize: 20,
+                fontFamily: 'Inconsolata , Arial',
+                fontColor: "#000",
             },
             scales: {
                 yAxes: [{
@@ -59,145 +48,85 @@ function trackersPerWeek() {
     })
 
     document.getElementById("week").style.minHeight = "350px";
-    // Hana ta del kode skrbi za to da je stvar responsive. 
+
 
     var ctx = document.getElementById('month');
     var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['https://Amazon.com', 'https://GitHub.com', 'https://StackOverflow.com', 'https://24ur.com', 'https://Facebook.com', 'https://Siol.si', 'https://Twitter.com', 'https://Feri.com'],
-            datasets: [{
-                label: '8 Most common trackers in the last month',
-                data: [384.00, 172.00, 400.00, 100.00, 420.000, 69.000, 666.000, 1.000],
+        type: 'bar',
+            data: {
+                labels: ["tracker1", "tracker2", "tracker3", "tracker4","tracker5","tracker6"],
+                datasets: [{
+                    label: '6 Most common trackers in the last month',
+                    data: [84.00, 72.00, 40.00, 40.00, 66.000, 63.000],
                 backgroundColor: [
-                    'rgba(216, 27, 96, 0.6)',
-                    'rgba(3, 169, 244, 0.6)',
-                    'rgba(255, 152, 0, 0.6)',
-                    'rgba(29, 233, 182, 0.6)',
-                    'rgba(156, 39, 176, 0.6)',
-                    'rgba(84, 110, 122, 0.6)'
-                ],
-                borderColor: [
-                    'rgba(216, 27, 96, 1)',
-                    'rgba(3, 169, 244, 1)',
-                    'rgba(255, 152, 0, 1)',
-                    'rgba(29, 233, 182, 1)',
-                    'rgba(156, 39, 176, 1)',
-                    'rgba(84, 110, 122, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            maintainAspectRatio: true,
-            legend: {
-                display: false,
-                //position: "bottom",
-                //fontSize: 20
-            },
-            title: {
-                display: true,
-                text: '8 Most common trackers in the last month',
-                position: 'top',
-                fontSize: 30,
-                fontFamily: 'Inconsolata , Arial, sans-serif',
-                fontColor: "#333333",
-            }
-        }
-    })
-
-    var ctx = document.getElementById('domain');
-    var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['https://facebook.com', 'https://google.com', 'https://yahoo.com', 'https://amazon.de', 'https://google.si', 'https://amazon.de'],
-            datasets: [{
-                label: 'Trackers active on domain',
-                data: [6, 7, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            maintainAspectRatio: true,
-            legend: {
-                position: "bottom",
-                fontSize: 20
-            },
-            title: {
-                display: true,
-                text: '8 Most common trackers in the last month',
-                position: 'top',
-                fontSize: 30,
-                fontFamily: 'Inconsolata , Arial, sans-serif',
-                fontColor: "#333333",
-            }
-        }
-    });
-
-    var ctx = document.getElementById('frequent');
-    var myChart = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: {
-            labels: ['https://facebook.com', 'https://google.com', 'https://yahoo.com', 'https://amazon.de', 'https://google.si', 'https://amazon.de'],
-            datasets: [{
-                label: 'Most frequent trackers',
-                data: [6, 7, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                minBarLength: 6
+                    'rgba(254,242,162,1)',
+                    'rgba(254,239,137,1)',
+                    'rgba(254,235,111,1)',
+                    'rgba(254,232,86,1)',
+                    'rgba(254,229,61,1)',
+                    'rgba(254,225,35,1)'
+                ]
             }]
         },
         options: {
             legend: {
                 display: false
             },
-            maintainAspectRatio: false,
+            maintainAspectRatio: true,
 
-            title: {
-                display: true,
-                text: 'Most frequent trackers',
-                position: 'top',
-                fontSize: 30,
-                fontFamily: 'Inconsolata , Arial, sans-serif',
-                fontColor: "#333333",
-            },
             scales: {
                 yAxes: [{
                     ticks: {
-                        min: 2
+                        min: 0
                     }
                 }]
+            },
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            title: {
+                display: true,
+                text: '8 Most common trackers in the last month',
+                position: 'top',
+                fontSize: 20,
+                fontFamily: 'Inconsolata , Arial, sans-serif',
+                fontColor: "#000",
+            }
+        }
+    })
+
+    var ctx = document.getElementById('allTime');
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ["tracker1", "tracker2", "tracker3", "tracker4","tracker5","tracker6"],
+            datasets: [{
+                label: 'Most frequent trackers',
+                data: [6, 7, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(254,242,162,1)',
+                    'rgba(254,239,137,1)',
+                    'rgba(254,235,111,1)',
+                    'rgba(254,232,86,1)',
+                    'rgba(254,229,61,1)',
+                    'rgba(254,225,35,1)'
+                ]
+            }]
+        },
+        options: {
+            maintainAspectRatio: true,
+            legend: {
+                display: false,
+            },
+            title: {
+                display: true,
+                text: '6 most frequently seen trackers off all time ',
+                position: 'top',
+                fontSize: 20,
+                fontFamily: 'Inconsolata , Arial, sans-serif',
+                fontColor: "#000"
             }
         }
     })
